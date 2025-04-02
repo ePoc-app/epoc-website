cf8ce5dae8adc48cc259d519aa947e15360550da
Benoit Rospars <benoit.rospars@inria.fr>
2025-04-02 09:50:16 +0200

    Add matomo and working newsletter subscription

--- components/content/NewsletterBox.vue
+++ components/content/NewsletterBox.vue
@@ -11,12 +11,32 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { toast } from 'vue-sonner'
+
+const { locale, defaultLocale } = useI18nDocs();
+const { proxy } = useScriptMatomoAnalytics();
 
 const email = ref("");
 
 const handleSubmit = () => {
     // Handle the subscription logic here
-    console.log("Subscribed with email:", email.value);
+    const prevEmails = JSON.parse(localStorage.getItem('newsletterEmails')||'[]');
+    if (!email.value && !proxy._paq) return;
+    if (prevEmails.indexOf(email.value) !== -1) {
+      toast.warning('Your email is already subscribed', {
+        description: email.value,
+        richColors: true
+      });
+      return;
+    }
+    const eventName = `Subscribe-${locale.value === defaultLocale ? locale.value : ''}`
+    proxy._paq.push(['trackEvent', 'Newsletter', eventName, email.value]);
+    toast.success('Your email has been added',{
+      description: email.value,
+      richColors: true
+    });
+    prevEmails.push(email.value);
+    localStorage.setItem('newsletterEmails', JSON.stringify(prevEmails))
 };
 defineProps<{
     placeholder: string;
