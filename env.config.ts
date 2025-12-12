b8dc2f0950444a534dd61eb42a4acd1b6d665f8c
Benoit Rospars <benoit.rospars@inria.fr>
2025-12-12 15:03:03 +0100

    chore: update release

--- env.config.ts
+++ env.config.ts
@@ -3,8 +3,8 @@ export default {
     mobile_version: 'v1.11.1',
     apple_link: 'https://apps.apple.com/app/epoc/id1596317383',
     google_link: 'https://play.google.com/store/apps/details?id=fr.inria.epoc',
-    editor_version: 'v0.3.0',
-    editor_link_windows: 'https://github.com/ePoc-App/epoc-editor/releases/download/v0.3.0/epoc-editor_v0.3.0_win_x64.exe',
-    editor_link_macos: 'https://github.com/ePoc-App/epoc-editor/releases/download/v0.3.0/epoc-editor_v0.3.0_mac_x64.dmg',
-    editor_link_linux: 'https://github.com/ePoc-App/epoc-editor/releases/download/v0.3.0/epoc-editor_v0.3.0_linux_x86_64.AppImage',
+    editor_version: 'v0.3.1',
+    editor_link_windows: 'https://github.com/ePoc-App/epoc-editor/releases/download/v0.3.1/epoc-editor_v0.3.1_win_x64.exe',
+    editor_link_macos: 'https://github.com/ePoc-App/epoc-editor/releases/download/v0.3.1/epoc-editor_v0.3.1_mac_x64.dmg',
+    editor_link_linux: 'https://github.com/ePoc-App/epoc-editor/releases/download/v0.3.1/epoc-editor_v0.3.1_linux_x86_64.AppImage',
 }
