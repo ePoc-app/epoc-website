954513e61e1461582b4bfffdd5b769c3e6a18e59
Benoit Rospars <benoit.rospars@inria.fr>
2026-02-16 10:36:17 +0100

    Bump mobile version to v2.0.1

--- env.config.ts
+++ env.config.ts
@@ -1,6 +1,6 @@
 export default {
     repo: 'https://github.com/ePoc-app',
-    mobile_version: 'v2.0.0',
+    mobile_version: 'v2.0.1',
     apple_link: 'https://apps.apple.com/app/epoc/id1596317383',
     google_link: 'https://play.google.com/store/apps/details?id=fr.inria.epoc',
     editor_version: 'v0.4.0',
